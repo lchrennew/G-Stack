50e17e9e17a79b40a42a450bd06e070eb3b268c8
lichun@agilean.cn <lichun@agilean.cn>
2018-07-13 16:53:45 +0800

    1、调用真实api 2、IndexProvider防页面闪烁 3、ParentLink链接地址计算 4、Item链接地址计算 5、后台支持跨域

--- gstack-console/src/main/resources/static/js/src/components/DirItem.jsx
+++ gstack-console/src/main/resources/static/js/src/components/DirItem.jsx
@@ -3,27 +3,46 @@ import React from 'react'
 import {Link, withRouter} from "react-router-dom";
 
 class DirItem extends React.Component {
+    buildLink(prefix) {
+        let {match: {isExact, params: {dir}, url}, location: {pathname}, name, itemtype} = this.props
+
+        let body = dir ?
+            isExact ?
+                [dir, name] :
+                [dir, pathname.substr(url.length).replace('/', ''), name] :
+            [name]
+        prefix.push(...body)
+        return prefix.join('/')
+    }
 
     getLink() {
-        let {match: {isExact, params: {dir, suite}, url}, location: {pathname}, name, itemtype} = this.props
+        let {match: {params: {suite}}, itemtype} = this.props
         let branch = itemtype === 'document' ? 'clob' : 'tree'
-        return dir ?
-            isExact ?
-                ['', suite, branch, dir, name].join('/') :
-                ['', suite, branch, dir, pathname.substr(url.length).replace('/', ''), name].join('/') :
-            ['', suite, branch, name].join('/')
+        return this.buildLink(['', suite, branch])
+    }
+
+    execute(e) {
+        e.preventDefault();
+        let path = this.buildLink(['.'])
+        alert(path)
+
+        let {match: {params: {suite}}} = this.props
+        alert(suite)
     }
 
     render() {
         // itemtype: folder => /tree/**
         // itemtype: document => /clob/**
         let {name, itemtype} = this.props
-
+        let link = this.getLink()
         return <tr>
             <th scope="row" className="icon"><i className={`icon ion-ios-${itemtype}`}/></th>
-            <td className="content"><Link to={this.getLink()}>{name}</Link></td>
+            <td className="content"><Link to={link}>{name}</Link></td>
             <td className="message">&nbsp;</td>
-            <td className="actions">&nbsp;</td>
+            <td className="actions">
+                <a href="#" className="link" onClick={this.execute.bind(this)}><i className="icon ion-md-play"></i></a>
+                <a href="#" className="link"><i className="icon ion-md-list"></i></a>
+            </td>
         </tr>
     }
 }
